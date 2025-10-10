30d0bd8cc0419841d1c1e8d31dec64572be3f0f4
Aryan <aryanshafierad@gmail.com>
2025-10-11 00:20:06 +0330

    Create sever.js

---
+++ back-end/sever.js
@@ -0,0 +1,9 @@
+// server.js
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/health', (req, res) => res.json({ status: 'ok' }));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log('Server running on', PORT));
